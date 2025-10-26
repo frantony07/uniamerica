f325356cf54e41a23362e4eb8cfe2f1b60ec1f20
Alex Dias <alex.dias.gold@gmail.com>
2025-10-26 15:51:11 -0300

    consertar css

--- script/cards.js
+++ script/cards.js
@@ -15,7 +15,7 @@ function loadPokemons() {
                 const card = `
                     <div class="card">
                         <div class="card-header">
-                            <h5 class="card-title text-center">${pokemon.name}</h5>
+                            <h5 id="card-title">${pokemon.name}</h5>
                         </div>
                         <div class="card-body">
                             <img src="${pokemon.photo_url}" alt="${pokemon.name}">
